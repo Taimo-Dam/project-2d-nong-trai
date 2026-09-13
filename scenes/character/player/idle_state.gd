76ef8f7031076f1fc915dd92d46622de2eba6ebd
Taimo-Dam <chickenvn003@gmail.com>
2026-09-13 19:27:30 +0700

    add player features

--- scenes/character/player/idle_state.gd
+++ scenes/character/player/idle_state.gd
@@ -1,25 +1,24 @@
 extends NodeState
 
-@export var player: CharacterBody2D
+@export var player: Player
 @export var animated_sprite_2d: AnimatedSprite2D
 
-var direction
 
 func _on_process(_delta : float) -> void:
 	pass
 
 
 func _on_physics_process(_delta: float) -> void:
-	direction = GameInputEvents.movement_input()
-
-	if direction == Vector2.UP:
+	if player.player_direction == Vector2.UP:
 		animated_sprite_2d.play("idle_back")
-	elif direction == Vector2.RIGHT:
+	elif player.player_direction == Vector2.RIGHT:
 		animated_sprite_2d.play("idle_right")
-	elif direction == Vector2.DOWN:
-		animated_sprite_2d.play("idle_front")
-	elif direction == Vector2.LEFT:
+	elif player.player_direction == Vector2.LEFT:
 		animated_sprite_2d.play("idle_left")
+	elif player.player_direction == Vector2.DOWN:
+		animated_sprite_2d.play("idle_front")
+	else: 
+		animated_sprite_2d.play("idle_front")
 
 
 func _on_next_transitions() -> void:
@@ -27,11 +26,16 @@ func _on_next_transitions() -> void:
 	
 	if GameInputEvents.is_movement_input():
 		transition.emit("Walk")
-
+	if player.current_tools == DataTypes.Tools.AxeWood && GameInputEvents.use_tool():
+		transition.emit("Chopping")
+	if player.current_tools == DataTypes.Tools.TillGround && GameInputEvents.use_tool():
+		transition.emit("Tilling")
+	if player.current_tools == DataTypes.Tools.WaterCrops && GameInputEvents.use_tool():
+		transition.emit("Watering")
 
 func _on_enter() -> void:
 	pass
 
 
 func _on_exit() -> void:
-	pass
+	animated_sprite_2d.stop()
--- scenes/character/player/walk_state.gd
+++ scenes/character/player/walk_state.gd
@@ -1,6 +1,6 @@
 extends NodeState
 
-@export var player: CharacterBody2D
+@export var player: Player
 @export var animated_sprite_2d: AnimatedSprite2D
 @export var speed: int = 70
 
@@ -20,7 +20,10 @@ func _on_physics_process(_delta: float) -> void:
 		animated_sprite_2d.play("walk_left")
 	elif  direction == Vector2.RIGHT:
 		animated_sprite_2d.play("walk_right")
-
+	
+	if direction != Vector2.ZERO:
+		player.player_direction = direction
+	
 	player.velocity = direction * speed
 	player.move_and_slide()
 
@@ -35,4 +38,4 @@ func _on_enter() -> void:
 
 
 func _on_exit() -> void:
-	pass
+	animated_sprite_2d.stop()
